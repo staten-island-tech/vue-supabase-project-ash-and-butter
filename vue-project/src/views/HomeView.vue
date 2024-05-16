b9b6d14a57e5b76b2d83b064d92d89e82f8df398
Batyrkhan Alimzhanov <batyrkhansa@gmail.com>
2024-05-16 09:07:26 -0400

    setup

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 import LoginMain from '../components/LoginMain.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <LoginMain />
   </main>
 </template>
