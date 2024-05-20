7eae5a8a447a6766650231c35bda0b92bbb7a317
Batyrkhan Alimzhanov <batyrkhansa@gmail.com>
2024-05-20 08:59:12 -0400

    3

--- vue-project/src/components/Auth.vue
+++ vue-project/src/components/Auth.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref } from 'vue'
 import { supabase } from '../supabase'
 
@@ -41,4 +41,4 @@ const handleLogin = async () => {
       </div>
     </div>
   </form>
-</template>
+</template> -->
--- vue-project/src/components/LoginMain.vue
+++ vue-project/src/components/LoginMain.vue
@@ -1,14 +1,20 @@
 <template>
-    <form @submit.prevent="login">
-      <label for="email">Email:</label>
-      <input type="email" v-model="email" required>
-      <label for="password">Password:</label>
-      <input type="password" v-model="password" required>
-      <button type="submit">Login</button>
-    </form>
-  </template>
+  <div class="1dh">
+    <h1>hi</h1>
+    <div class="2dh3dAL">
+      <h2>hi</h2>
+    </div>
+  </div>
+  <form @submit.prevent="login">
+    <label for="email">Email:</label>
+    <input type="email" v-model="email" required>
+    <label for="password">Password:</label>
+    <input type="password" v-model="password" required>
+    <button type="submit">Login</button>
+  </form>
+</template>
   
-  <script>
+  <!-- <script>
   import { ref } from 'vue';
   import { supabase } from '../supabase.js';
   
@@ -39,4 +45,12 @@
       };
     },
   };
-  </script>
+  </script> -->
+<style scoped>
+.1dh {
+  position: stat;
+}
+
+
+</style>
+
